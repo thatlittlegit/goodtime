79080d602822ea0a6e72ff9b5ab5e319b3af775f
thatlittlegit <personal@thatlittlegit.tk>
2019-12-22 15:11:29 -0500

    Clean up global variables and #defines
    This should ease translation into Vala. It also seemed like a code smell
    when I put them in, so I'm happy that they've been dealt with.
    
    The only remaining globals are:
      * stillopen, which can probably be removed further into the Vala
        translation
      * set_time, which will probably become a static variable in Vala.
    
    There is also widget_picker_pair introduced, which I hope to replace
    with something along the lines of a tuple.

--- goodtime.c
+++ goodtime.c
@@ -5,24 +5,24 @@
 
 #define GLADEOBJ(x) gtk_builder_get_object(builder, x)
 #define GraniteTimePicker GraniteWidgetsTimePicker
-#define GTCLAMP(x, y) (x > y ? y : x)
 
 #define CLEAR_SECONDS_IN_GDATETIME(gdt) g_date_time_new_from_unix_local(g_date_time_to_unix(gdt) - g_date_time_get_second(gdt))
 #define UNUSED(x) (void)(x)
-#define MICROSECONDS_IN_DECADE 315576000000000
 
-const struct timespec poll_interval = { 0, 1000 };
-GtkWindow* window;
-GraniteTimePicker* picker;
 bool stillopen = true;
 GDateTime* set_time = NULL;
 
+struct widget_picker_pair {
+    GtkWidget* widget;
+    GraniteTimePicker* picker;
+};
+
 // Implemented here
-static void update(GraniteTimePicker* IGNORED, gpointer _IGNORED);
-static gboolean routine_update(gpointer IGNORED);
+static void update(GraniteTimePicker* IGNORED, GtkWindow* window);
+static gboolean routine_update(gpointer window);
 void gt_activate(GApplication* application, gpointer IGNORED);
 bool get_still_open();
-static void update_time(GtkButton* IGNORED, gpointer headerbar);
+static void update_time(GtkButton* IGNORED, gpointer window_picker_pair);
 static void show_popover(GtkButton* IGNORED, gpointer popover);
 
 // Implemented in Vala
@@ -36,11 +36,9 @@ bool get_still_open()
     return stillopen;
 }
 
-static void update(GraniteTimePicker* _, gpointer __)
+static void update(GraniteTimePicker* _, GtkWindow* window)
 {
     UNUSED(_);
-    UNUSED(__);
-
     GtkLabel* label = GTK_LABEL(gtk_bin_get_child(GTK_BIN(window)));
     if (set_time == NULL) {
         gtk_label_set_text(label, "+??:??:??");
@@ -57,22 +55,22 @@ static void update(GraniteTimePicker* _, gpointer __)
     }
 }
 
-static gboolean routine_update(gpointer _)
+static gboolean routine_update(gpointer window)
 {
-    UNUSED(_);
-    update(picker, window);
+    update(NULL, GTK_WINDOW(window));
     return TRUE;
 }
 
-static void update_time(GtkButton* _, gpointer headerbar)
+static void update_time(GtkButton* _, gpointer _wpp)
 {
+    struct widget_picker_pair* wpp = _wpp;
     UNUSED(_);
-    set_time = CLEAR_SECONDS_IN_GDATETIME(granite_widgets_time_picker_get_time(picker));
+    set_time = CLEAR_SECONDS_IN_GDATETIME(granite_widgets_time_picker_get_time(wpp->picker));
 
     char newSubtitle[32];
-    GDateTime* time = CLEAR_SECONDS_IN_GDATETIME(granite_widgets_time_picker_get_time(picker));
+    GDateTime* time = CLEAR_SECONDS_IN_GDATETIME(granite_widgets_time_picker_get_time(wpp->picker));
     snprintf(newSubtitle, 31, "time until %s", g_date_time_format(time, "%R"));
-    gtk_header_bar_set_subtitle(GTK_HEADER_BAR(headerbar), newSubtitle);
+    gtk_header_bar_set_subtitle(GTK_HEADER_BAR(wpp->widget), newSubtitle);
 }
 
 static void show_popover(GtkButton* _, gpointer popover)
@@ -86,16 +84,19 @@ void gt_activate(GApplication* app, gpointer userdata)
     UNUSED(userdata);
 
     GtkBuilder* builder = gtk_builder_new_from_file("goodtime.glade");
-    GObject* _window = GLADEOBJ("window");
-    window = GTK_WINDOW(_window);
+    GtkWindow* window = GTK_WINDOW(GLADEOBJ("window"));
     gtk_application_add_window(GTK_APPLICATION(app), window);
 
-    picker = granite_widgets_time_picker_new();
+    GraniteTimePicker* picker = granite_widgets_time_picker_new();
     gtk_box_pack_start(GTK_BOX(GLADEOBJ("popover-container")), GTK_WIDGET(picker), TRUE, TRUE, 0);
     g_signal_connect(GLADEOBJ("show-popover"), "clicked", G_CALLBACK(show_popover), GLADEOBJ("popover"));
-    g_signal_connect(GLADEOBJ("accept-new-time"), "clicked", G_CALLBACK(update_time), GLADEOBJ("headerbar"));
     gtk_widget_show(GTK_WIDGET(picker));
     gtk_widget_show(GTK_WIDGET(window));
 
-    g_timeout_add_seconds(1, routine_update, NULL);
+    struct widget_picker_pair* wpp = malloc(sizeof(struct widget_picker_pair));
+    wpp->widget = GTK_WIDGET(GLADEOBJ("headerbar"));
+    wpp->picker = picker;
+    g_signal_connect(GLADEOBJ("accept-new-time"), "clicked", G_CALLBACK(update_time), wpp);
+
+    g_timeout_add_seconds(1, routine_update, window);
 }
