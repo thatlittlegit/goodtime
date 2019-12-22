45d964102c1163c4628710e2234e313bc6539349
thatlittlegit <personal@thatlittlegit.tk>
2019-12-22 13:28:11 -0500

    Add a confirm button for timesetting
    This commit also moves headerbar setting to the confirm-handling section
    and adds the possibility for a '+??:??:??' label if the time isn't set.

--- goodtime.c
+++ goodtime.c
@@ -15,12 +15,15 @@ const struct timespec poll_interval = { 0, 1000 };
 GtkWindow* window;
 GraniteTimePicker* picker;
 bool stillopen = true;
+GDateTime* set_time = NULL;
 
 // Implemented here
 static void update(GraniteTimePicker* IGNORED, gpointer _IGNORED);
 static gboolean routine_update(gpointer IGNORED);
 void gt_activate(GApplication* application, gpointer IGNORED);
 bool get_still_open();
+static void update_time(GtkButton* IGNORED, gpointer headerbar);
+static void show_popover(GtkButton* IGNORED, gpointer popover);
 
 // Implemented in Vala
 gpointer play_sound(gpointer IGNORED);
@@ -38,15 +41,15 @@ static void update(GraniteTimePicker* _, gpointer __)
     UNUSED(_);
     UNUSED(__);
 
-    char newSubtitle[32];
-    GDateTime* time = CLEAR_SECONDS_IN_GDATETIME(granite_widgets_time_picker_get_time(picker));
-    snprintf(newSubtitle, 31, "time until %s", g_date_time_format(time, "%R"));
-    GtkWidget* headerbar = gtk_window_get_titlebar(window);
-    gtk_header_bar_set_subtitle(GTK_HEADER_BAR(headerbar), newSubtitle);
+    GtkLabel* label = GTK_LABEL(gtk_bin_get_child(GTK_BIN(window)));
+    if (set_time == NULL) {
+        gtk_label_set_text(label, "+??:??:??");
+        return;
+    }
 
-    int span = g_date_time_difference(time, g_date_time_new_now_local());
+    int span = g_date_time_difference(set_time, g_date_time_new_now_local());
     char* timeuntil = timespan_to_string(span);
-    gtk_label_set_text(GTK_LABEL(gtk_bin_get_child(GTK_BIN(window))), timeuntil);
+    gtk_label_set_text(label, timeuntil);
     free(timeuntil);
 
     if (span < 0 && span >= -G_TIME_SPAN_SECOND) {
@@ -61,6 +64,23 @@ static gboolean routine_update(gpointer _)
     return TRUE;
 }
 
+static void update_time(GtkButton* _, gpointer headerbar)
+{
+    UNUSED(_);
+    set_time = CLEAR_SECONDS_IN_GDATETIME(granite_widgets_time_picker_get_time(picker));
+
+    char newSubtitle[32];
+    GDateTime* time = CLEAR_SECONDS_IN_GDATETIME(granite_widgets_time_picker_get_time(picker));
+    snprintf(newSubtitle, 31, "time until %s", g_date_time_format(time, "%R"));
+    gtk_header_bar_set_subtitle(GTK_HEADER_BAR(headerbar), newSubtitle);
+}
+
+static void show_popover(GtkButton* _, gpointer popover)
+{
+    UNUSED(_);
+    gtk_popover_popup(GTK_POPOVER(popover));
+}
+
 #define activate gt_activate
 void gt_activate(GApplication* app, gpointer userdata)
 {
@@ -72,8 +92,9 @@ void gt_activate(GApplication* app, gpointer userdata)
     gtk_application_add_window(GTK_APPLICATION(app), window);
 
     picker = granite_widgets_time_picker_new();
-    gtk_header_bar_pack_start(GTK_HEADER_BAR(GLADEOBJ("headerbar")), GTK_WIDGET(picker));
-    g_signal_connect(picker, "time_changed", G_CALLBACK(update), window);
+    gtk_box_pack_start(GTK_BOX(GLADEOBJ("popover-container")), GTK_WIDGET(picker), TRUE, TRUE, 0);
+    g_signal_connect(GLADEOBJ("show-popover"), "clicked", G_CALLBACK(show_popover), GLADEOBJ("popover"));
+    g_signal_connect(GLADEOBJ("accept-new-time"), "clicked", G_CALLBACK(update_time), GLADEOBJ("headerbar"));
     gtk_widget_show(GTK_WIDGET(picker));
     gtk_widget_show(GTK_WIDGET(window));
 
